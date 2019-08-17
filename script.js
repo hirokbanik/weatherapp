79c8fb3ca69d1a08cc05caa8b9a5f3c5415596ac
HIROK BANIK <hirokbanik@gmail.com>
2019-08-18 01:50:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let loc = select(".location"),
-  currentTemp = select(".current-temp"),
-  minTemp = select(".min-temp"),
-  maxTemp = select(".max-temp"),
-  weather = select(".weather"),
-  icon = select(".icon"),
-  cloud = select(".cloud .value"),
-  humidity = select(".humidity .value"),
-  wind = select(".wind .value"),
-  form = select("form");
+let loc = select(".location");
+let currentTemp = select(".current-temp");
+let minTemp = select(".min-temp");
+let maxTemp = select(".max-temp");
+let weather = select(".weather");
+let icon = select(".icon");
+let cloud = select(".cloud .value");
+let humidity = select(".humidity .value");
+let wind = select(".wind .value");
 
 getData(localStorage.getItem("city"));
 
@@ -15,41 +14,58 @@ setInterval(() => {
   getData(localStorage.getItem("city"));
 }, 1000 * 30);
 
-form.addEventListener("submit", e => {
+loc.addEventListener("change", e => {
   e.preventDefault();
-  localStorage.setItem("city", form.city.value);
-  getData(form.city.value);
-  form.reset();
+  localStorage.setItem("city", loc.value);
+  getData(loc.value);
+  loc.value = "";
 });
 
-function select(el) {
-  return document.querySelector(el);
-}
-
-function getData(cityCode = "Silchar") {
+function getData(cityCode) {
   const baseURL = "https://api.openweathermap.org/data/2.5/weather";
   const API_KEY = "8796a327bb73a0360ec025153ce7b207";
   const unit = "metric";
-  const city = cityCode;
-  let url = `${baseURL}?q=${encodeURIComponent(
-    city
-  )}&appid=${API_KEY}&units=${unit}`;
+  const city = encodeURIComponent(cityCode || "hailakandi");
+  let url = `${baseURL}?q=${city}&appid=${API_KEY}&units=${unit}`;
 
   fetch(url)
     .then(res => res.json())
-    .then(res => {
-      let data = res;
-      console.log(data.name);
-      loc.innerText = `${data.name}, ${data.sys.country}`;
-      currentTemp.innerText = `${data.main.temp.toFixed(0)}°C`;
-      minTemp.innerText = `Min ${data.main.temp_min.toFixed(0)}°C`;
-      maxTemp.innerText = `Max ${data.main.temp_max.toFixed(0)}°C`;
-      weather.innerText = data.weather[0].description;
-      cloud.innerText = `${data.clouds.all}%`;
-      humidity.innerText = `${data.main.humidity}%`;
-      wind.innerText = `${data.wind.speed} m/s`;
-      icon.src = `https://openweathermap.org/img/wn/${
-        data.weather[0].icon
-      }@2x.png`;
-    });
+    .then(updateData);
+}
+
+function updateData(res) {
+  if (res.cod == 200) {
+    loc.value = `${res.name}, ${res.sys.country}`;
+    currentTemp.innerText = `${res.main.temp.toFixed(0)}°C`;
+    minTemp.innerText = `Min ${res.main.temp_min.toFixed(0)}°C`;
+    maxTemp.innerText = `Max ${res.main.temp_max.toFixed(0)}°C`;
+    weather.innerText = res.weather[0].description;
+    cloud.innerText = `${res.clouds.all}%`;
+    humidity.innerText = `${res.main.humidity}%`;
+    wind.innerText = `${res.wind.speed} m/s`;
+    icon.src = `./icons/${res.weather[0].icon}.svg`;
+    showElements();
+  } else {
+    loc.value = "Oops.." + res.message;
+    hideElements();
+  }
+}
+
+// HELPER FUNCTIONS
+function select(el) {
+  let elements = document.querySelectorAll(el);
+  if (elements.length == 1) return elements[0];
+  else return elements;
+}
+
+function hideElements() {
+  select(".location ~ *").forEach(i => {
+    i.style.opacity = "0";
+  });
+}
+
+function showElements() {
+  select(".location ~ *").forEach(i => {
+    i.style.opacity = "1";
+  });
 }
